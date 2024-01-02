043f71a9723abd92644655fa304fc0fe2f8bb93e
Owen Williams <williams.owen@gmail.com>
2024-01-02 21:41:30 +0000

    adding cybergear as a variant

---
+++ variants/XIAOMI_CYBERGEAR/PinNamesVar.h
@@ -0,0 +1,24 @@
+#ifndef _PINNAMESVAR_H_
+#define _PINNAMESVAR_H_
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+// aliases
+#define A_INHA PA8
+#define A_INHB PA9
+#define A_INHC PA10
+#define A_EN_DRV PA11
+#define A_NFAULT PA12
+
+#define A_CSOC PC0
+#define A_CSOB PC1
+
+#define A_LED1 PA5
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* _PINNAMESVAR_H_ */
