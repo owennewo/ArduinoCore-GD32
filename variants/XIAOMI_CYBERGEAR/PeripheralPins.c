1c8e510f076c3ccb235e27b036ca640088161326
Owen Williams <williams.owen@gmail.com>
2024-02-05 17:54:50 +0000

    tweaks to pins

--- variants/XIAOMI_CYBERGEAR/PeripheralPins.c
+++ variants/XIAOMI_CYBERGEAR/PeripheralPins.c
@@ -303,30 +303,30 @@ const PinMap PinMap_UART_CTS[] = {
 
 /* SPI PinMap */
 const PinMap PinMap_SPI_MOSI[] = {
-    {PORTA_7, SPI0, 7},
-    {PORTB_5, SPI0, 7 | (1 << 3)}, /* GPIO_SPI0_REMAP */
-    {PORTB_15, SPI1, 7},
+    // {PORTA_7, SPI0, 7},
+    // {PORTB_5, SPI0, 7 | (1 << 3)}, /* GPIO_SPI0_REMAP */
+    // {PORTB_15, SPI1, 7},
     {PORTC_12, SPI2, 7 | (30 << 3)}, /* GPIO_SPI2_REMAP */
     {NC, NC, 0}};
 
 const PinMap PinMap_SPI_MISO[] = {
-    {PORTA_6, SPI0, 3},
-    {PORTB_4, SPI0, 3 | (1 << 3)}, /* GPIO_SPI0_REMAP */
-    {PORTB_14, SPI1, 3},
-    {PORTC_11, SPI2, 3 | (30 << 1)}, /* GPIO_SPI2_REMAP */
+    // {PORTA_6, SPI0, 3},
+    // {PORTB_4, SPI0, 3 | (1 << 3)}, /* GPIO_SPI0_REMAP */
+    // {PORTB_14, SPI1, 3},
+    {PORTC_11, SPI2, 1 | (30 << 3)}, /* GPIO_SPI2_REMAP */
     {NC, NC, 0}};
 
 const PinMap PinMap_SPI_SCLK[] = {
-    {PORTA_5, SPI0, 7},
-    {PORTB_3, SPI0, 7 | (1 << 3)}, /* GPIO_SPI0_REMAP */
-    {PORTB_13, SPI1, 7},
+    // {PORTA_5, SPI0, 7},
+    // {PORTB_3, SPI0, 7 | (1 << 3)}, /* GPIO_SPI0_REMAP */
+    // {PORTB_13, SPI1, 7},
     {PORTC_10, SPI2, 7 | (30 << 3)}, /* GPIO_SPI2_REMAP */
     {NC, NC, 0}};
 
 const PinMap PinMap_SPI_SSEL[] = {
-    {PORTA_4, SPI0, 7 | (30 << 3)}, /* GPIO_SPI2_REMAP*/
-    {PORTA_15, SPI0, 7},            /* GPIO_SPI2_REMAP - NO REMAP*/
-    {PORTB_12, SPI1, 7},
+    // {PORTA_4, SPI0, 7 | (30 << 3)}, /* GPIO_SPI2_REMAP*/
+    {PORTA_15, SPI0, 7}, /* GPIO_SPI2_REMAP - NO REMAP*/
+    // {PORTB_12, SPI1, 7},
     {NC, NC, 0}};
 
 /* CAN PinMap */
